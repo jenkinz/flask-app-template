51ef27233dd94a4fd5fec9f1125a077824fbff39
Brian Jenkins <bj@jenkinz.com>
2022-04-20 16:32:08 -0700

    Rename package to flaskr and update black version

--- src/python_project_template/player.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    pass
-
-
-if __name__ == "__main__":  # pragma: no cover
-    Player()
-    print("Created a Player.")
--- tests/test_player.py
+++
@@ -1,5 +0,0 @@
-from python_project_template.player import Player
-
-
-def test_construction():
-    assert Player()
